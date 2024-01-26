ee9475cf2811410b9109af0aa4e377c1fe07a39e
saikatbhuiyan <shaikotbhuiya@gmail.com>
2024-01-27 02:13:02 +0600

    Create simple provider

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,3 +1,4 @@
+import { CoffeesService } from './coffees.service';
 import {
   Body,
   Controller,
@@ -13,14 +14,17 @@ import {
 } from '@nestjs/common';
 
 import { Response } from 'express';
+import { Coffee } from './entites/coffee.entity';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   @Get()
   findAll(@Query() paginationQuery, @Res() response: Response) {
-    const { limit, offset } = paginationQuery;
+    // const { limit, offset } = paginationQuery;
 
-    response.status(200).send('This action returns all coffees');
+    response.status(200).send(this.coffeesService.findAll());
   }
 
   // @Get(':id')
@@ -29,24 +33,23 @@ export class CoffeesController {
   // }
 
   @Get(':id')
-  findOne(@Param('id') id: string): string {
-    return 'This action returns a single coffees with id ' + id;
+  findOne(@Param('id') id: string): Coffee {
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() body) {
-    return body;
-    // return 'This action creates a new coffees';
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return body;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string, @Body() body) {
-    return body;
+  delete(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
   }
 }
