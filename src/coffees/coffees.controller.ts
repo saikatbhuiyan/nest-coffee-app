28418a8886bfa3daa883db1f0adcecacab6dc62c
saikatbhuiyan <shaikotbhuiya@gmail.com>
2024-01-26 14:41:30 +0600

    Simple route

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,19 +1,52 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+
+import { Response } from 'express';
 
 @Controller('coffees')
 export class CoffeesController {
-    @Get()
-    findAll(): string {
-      return 'This action returns all coffees';
-    }
+  @Get()
+  findAll(@Query() paginationQuery, @Res() response: Response) {
+    const { limit, offset } = paginationQuery;
 
-    // @Get(':id')
-    // findOne(@Param() params): string {
-    //     return 'This action returns a single coffees with id ' + params.id
-    // }
+    response.status(200).send('This action returns all coffees');
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string): string {
-        return 'This action returns a single coffees with id ' + id
-    }
+  // @Get(':id')
+  // findOne(@Param() params): string {
+  //     return 'This action returns a single coffees with id ' + params.id
+  // }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return 'This action returns a single coffees with id ' + id;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() body) {
+    return body;
+    // return 'This action creates a new coffees';
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return body;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string, @Body() body) {
+    return body;
+  }
 }
