a4de0647d550b7e10dbfb4a26aa38ca5b48b98d4
shahabuddindvive <shahabuddin.bhuiyan@dvive.uk>
2024-01-31 14:44:59 +0600

    Env config and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,25 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
+    }),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '172.20.192.1',
-      port: 5432,
-      username: 'postgres',
-      password: 'password1234',
-      database: 'coffee_bd',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
