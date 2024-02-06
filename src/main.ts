4599fe77b57b22faf7e1f25531e6d808b271eec4
shahabuddindvive <shahabuddin.bhuiyan@dvive.uk>
2024-02-06 07:21:34 +0600

    Added swagger doch

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 // import { ApiKeyGuard } from './common/guards/api-key.guard';
 
@@ -24,6 +25,15 @@ async function bootstrap() {
     new WrapResponseInterceptor(),
     new TimeoutInterceptor(),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('CoffeeScript')
+    .setDescription('Coffee application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
