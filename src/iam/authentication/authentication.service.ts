8376e64102c98f462e2902b4fa8f99498494c4a5
shahabuddindvive <shahabuddin.bhuiyan@dvive.uk>
2024-02-10 10:23:51 +0600

    Create auth services

---
+++ src/iam/authentication/authentication.service.ts
@@ -0,0 +1,55 @@
+import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { HashingService } from '../hashing/hashing.service';
+import { PG_UNIQUE_VIOLATION_ERROR_CODE } from 'src/common/constant/keys.constant';
+
+@Injectable()
+export class AuthenticationService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+    private readonly hashingService: HashingService,
+  ) {}
+
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.password = await this.hashingService.hash(signUpDto.password);
+
+      await this.usersRepository.save(user);
+    } catch (error) {
+      const pgUniqueViolationErrorCode = PG_UNIQUE_VIOLATION_ERROR_CODE;
+      if (error.code === pgUniqueViolationErrorCode) {
+        throw new ConflictException();
+      }
+      throw error;
+    }
+  }
+
+  async signIn(signInDto: SignInDto) {
+    const user = await this.usersRepository.findOneBy({
+      email: signInDto.email,
+    });
+    if (!user) {
+      throw new UnauthorizedException('User does not exists');
+    }
+
+    const isEqual = await this.hashingService.compare(
+      user.email,
+      signInDto.email,
+    );
+    if (!isEqual) {
+      throw new UnauthorizedException('Password does not match');
+    }
+
+    return true;
+  }
+}
